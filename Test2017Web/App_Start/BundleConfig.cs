fc0eb00fc783f13c619e77214f725f24faa6d354
Sultan Jared <sjarad@yahoo.com>
2019-10-26 17:34:56 -0500

    initial commit

---
+++ Test2017Web/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Test2017Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/js/lib/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+          "~/js/lib/bootstrap.min.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+                        "~/js/lib/jquery.dataTables.min.js",
+                        "~/js/lib/dataTables.bootstrap.min.js",
+                        "~/js/lib/dataTables.colReorder.min.js"));
+
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+
+                      "~/css/bootstrap.min.css",
+                      "~/css/jquery.dataTables.min.css",
+                      "~/css/dataTables.bootstrap4.min.css",
+                      "~/css/colReorder.bootstrap4.min.css",
+                      "~/css/site.css"));
+        }
+    }
+}
---
+++ Test2017Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Test2017Web.Startup))]
+namespace Test2017Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
