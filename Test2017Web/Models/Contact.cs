df6a64cbb71f9494a3f230ad2a2bf62b30894d63
Sultan Jared <sjarad@yahoo.com>
2019-10-28 22:46:51 -0500

    added new Contact.cs

---
+++ Test2017Web/Models/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test2017Web.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhonePrimary { get; set; }
+        public string PhoneSecondary { get; set; }
+        public DateTime Birthday { get; set; }
+        public string StreesAddress1 { get; set; }
+        public string StreesAddress2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+    }
+}
